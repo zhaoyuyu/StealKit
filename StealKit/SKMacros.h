64cc68976c79b5b8c8730bc0f3f321d09c15783b
zhaoyuyu <zhaoyuyu1@126.com>
2016-08-29 17:40:32 +0800

    remove macro style version comparion
    remove macro style version comparion

--- StealKit/SKMacros.h
+++ StealKit/SKMacros.h
@@ -19,21 +19,6 @@
 
 #define DEGREES_TO_RADIANS(degrees) degrees * M_PI / 180
 
-#define SYSTEM_VERSION_EQUAL_TO(v)  \
-    ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
-
-#define SYSTEM_VERSION_GREATER_THAN(v) \
-    ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
-
-#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v) \
-    ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
-
-#define SYSTEM_VERSION_LESS_THAN(v) \
-    ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
-
-#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v) \
-    ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
-
 #define UIColorFromRGB(rgbValue) \
     [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
                     green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
